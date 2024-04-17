e0188b8e816ae581a33f37030a5ae2859cba55f4
Emma Hubert <ehubert@email.sc.edu>
2024-04-17 13:30:53 -0400

    updt

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const showHideNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 const logoLink = () => {
-    window.location.href= "https://ehubertt.github.io/Projects/part3/home/index.html";
+    window.location.href= "https://ehubertt.github.io/Projects/final/final-pages/home/index.html";
 }
 
 document.getElementById("logo").onclick = logoLink;
